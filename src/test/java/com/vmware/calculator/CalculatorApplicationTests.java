fd3207043d929971da1e610090e0eb0c35b1275d
Umarani <uumaran@apac.corpdir.net>
2020-03-25 18:16:20 +0530

    spring initializr project

---
+++ src/test/java/com/vmware/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
